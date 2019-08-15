0e86c0f20b89d56ece894af119cebd71242aaf16
mirzaasw <52796909+mirzaasw@users.noreply.github.com>
2019-08-15 11:02:41 -0400

    Add files via upload

---
+++ assets/data/motorcycleData.ts
@@ -0,0 +1,9 @@
+import {Motorcycle} from '../../app/motorcycle';
+
+export const MYMOTORCYLES:Motorcycle[]=[
+    
+    {model:'M1345',engine:'present',style:'cool'},
+    {model:'N1923',engine:'Electric',style:'semiCool'},
+    {model:'tra5h',engine:'not there',style:'lame'}
+
+];
